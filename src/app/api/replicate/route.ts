6dc782f5c5b58bd1b6f537b839ee12778924db55
Walaa VOILIDAS <walaavolidis@gmail.com>
2025-01-13 12:20:27 +0200

    Finish api and get image correctly

--- src/app/api/replicate/route.ts
+++ src/app/api/replicate/route.ts
@@ -1,5 +1,11 @@
 import { NextResponse, NextRequest } from 'next/server';
 import Replicate from 'replicate';
+import { writeFile } from 'node:fs/promises';
+import { z } from 'zod';
+
+const ZReplicateSchema = z.object({
+  imageUrl: z.string(),
+});
 
 const replicate = new Replicate({
   auth: process.env.REPLICATE_API_TOKEN,
@@ -8,33 +14,35 @@ const replicate = new Replicate({
 export async function POST(request: NextRequest) {
   try {
     const { prompt } = await request.json();
-    console.log('Prompt:', prompt);
 
-    const output = await replicate.run(
-      //'stability-ai/stable-diffusion-3',
-      'stability-ai/sdxl:39ed52f2a78e934b3ba6e2a89f5b1c712de7dfea535525255b1aa35c5565e08b',
-      {
-        input: {
-          prompt: prompt,
-          negative_prompt: 'low quality, bad anatomy, blurry, pixelated',
-          num_outputs: 1,
-          num_inference_steps: 50,
-          guidance_scale: 7.5,
-        },
-      }
-    );
+    const input = {
+      width: 768,
+      height: 768,
+      prompt: prompt,
+      refine: 'expert_ensemble_refiner',
+      apply_watermark: false,
+      num_inference_steps: 25,
+    };
 
-    console.log('Output:', output);
+    const output = await replicate.run('stability-ai/stable-diffusion-3', {
+      input,
+    });
 
-    return NextResponse.json({ output });
+    let imageUrl: string = '';
+
+    for (const [index, item] of Object.entries(output)) {
+      const filePath = `./public/${prompt}_${index}.png`;
+      await writeFile(filePath, item);
+      imageUrl = filePath.replace('./public', '');
+    }
+    ZReplicateSchema.parse({ imageUrl });
+    console.log('images', imageUrl);
+    return NextResponse.json({
+      message: 'Images generated and saved successfully',
+      imageUrl: imageUrl,
+    });
   } catch (error) {
     console.error('Error:', error);
-    return NextResponse.json(
-      { error: 'Failed to generate image' },
-      { status: 500 }
-    );
+    return NextResponse.json({ error: 'Failed to generate image' });
   }
 }
-
-
-
