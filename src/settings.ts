d1d8358bb9be45a68bbeab74f135921e33d60b06
Walaa VOILIDAS <walaavolidis@gmail.com>
2025-01-19 12:18:02 +0200

    create settings file

--- src/settings.ts
+++ src/settings.ts
@@ -2,8 +2,20 @@ import { z } from 'zod';
 
 export const ZServerSettings = z.object({
   replicateApiToken: z.string(),
+  region: z.string(),
+  accessKeyId: z.string(),
+  secretAccessKey: z.string(),
+  destBucket: z.string(),
+  s3CDN: z.string(),
+  endpoint: z.string(),
 });
 
 export const SERVER_SETTINGS = ZServerSettings.parse({
   replicateApiToken: process.env['REPLICATE_API_TOKEN'],
+  region: process.env['AWS_REGION'],
+  accessKeyId: process.env['ACCESS_KEY'],
+  secretAccessKey: process.env['SECRET_ACCESS_KEY'],
+  destBucket: process.env['DEST_BUCKET'],
+  s3CDN: process.env['S3_CDN_URL'],
+  endpoint: process.env['S3_ENDPOINT'],
 });
